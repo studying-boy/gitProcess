df8cb7a7ff7b5b09079b51728a006263fee59d2c
liquansheng <liquansheng@gridsum.com>
2022-07-20 11:39:41 +0800

    feat: 增加cz

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        ':sparkles:',
+        ':bug:',
+        ':ambulance:',
+        ':lipstick:',
+        ':construction:',
+        ':memo:',
+        ':bookmark:',
+        ':hammer:',
+        ':rocket:',
+        ':tada:',
+      ],
+    ],
+    'subject-case': [0, 'never'],
+  },
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(:\w*:)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
+      headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+    },
+  },
+};
